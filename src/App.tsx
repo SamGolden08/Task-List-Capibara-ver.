16b1bfd3ff3d1576c869ff8245f7234e41618db2
SamMidnight03 <ssgomezeliz@gmail.com>
2025-09-29 17:55:08 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import Task from "./Components/Task.jsx"
+import './App.css'
+
+function App() {
+
+  const [text, setText] = useState("");
+  const [task, setTask] = useState<string[]>([]);
+
+  //Poder subir la tarea con la tecla enter
+  function inputKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      AddTask();
+    }
+  }
+
+  //Actualiza el texto 
+  function ActualizarText(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  //Agrega la tarea y si el campo esta basio no se sube
+  function AddTask() {
+
+    if(text.trim() == "" ){
+      alert("Tarea basia")
+      return
+    }
+
+    setTask([...task, text]); //Se actualiza la tarea
+    localStorage.setItem("tareas", JSON.stringify([...task, text])) //se agrega como un .JSON al localStorage
+    setText(""); //Eliminamos lo escrito anteriormente para dejar el campo bacio para otras tareas
+  }
+  
+  //Elimina las tareas
+  function Eliminar(index: number){
+    setTask(task => {
+      const newTask = [...task] //Copiams el array
+      newTask.splice(index, 1) //Eliminaos el numero de index
+      localStorage.setItem("tareas", JSON.stringify(newTask)) //se actualiza local
+      return newTask // se da el nuevo arreglo
+    })
+  }
+
+  //subimos las cosas al local storage
+  useEffect(() => {
+    const tareasStorage = localStorage.getItem("tareas")
+    if (tareasStorage) {
+      setTask(
+        JSON.parse(tareasStorage) //convierte en un string a array
+      )
+    }
+  }, [])
+
+  return (
+  <main>
+      <div className='FormularioTask'>
+        <h1>Task List</h1>
+        <p>Agrega tu tarea pendiente</p>
+        <div className='InputTask'>
+          <input className='inputTask' type="text" placeholder='Agrega tu tarea'  onChange={ActualizarText} value={text} onKeyDown={inputKeyDown}/>
+          <button onClick={AddTask} >Submit</button>
+        </div>
+        <img src="./Capibara.png" alt="capibara" />
+      </div>
+
+      <div className='Tasks'>
+        <h2>Tareas</h2>
+        {task.map((task, index) => {
+          return <Task key={task} task={task} Eliminar={Eliminar} index={index}/>
+        })}
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Task.tsx
@@ -0,0 +1,28 @@
+//Creacion del tipo 
+type TaskProps = {
+  task: string;            // cada tarea es un string
+  Eliminar: (index: number) => void; // función que recibe un número
+  index: number;           // posición de la tarea
+};
+
+export default function Task({ task, Eliminar, index }: TaskProps) {
+  return (
+    <div className="Task">
+
+      <div className="TaskText">
+
+        <input type="checkbox" />
+        <p>{task}</p>
+
+      </div>
+
+      <div id="imagen" onClick={() => Eliminar(index)}>
+
+        <img src="./public/CapibaraTrash.png" alt="TrashCapibara" />
+
+      </div>
+
+    </div>
+  );
+}
+
